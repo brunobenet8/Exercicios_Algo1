6a427f059ca51a6c9e2945c955a3e754f4ed7b72
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-10 16:27:51 -0300

    Create Exemplo_decisao_5.pas
    Exemplo página 123 livro Algoritmo e Programação 1, UEPG;

---
+++ Exemplo_decisao_5.pas
@@ -0,0 +1,14 @@
+Program Exemplo_decisao_5 ;
+
+Var
+numero : integer;
+
+Begin
+write (‘Entre com um número positivo menor do que 100: ‘);
+read(numero);
+if ((numero >= 0) and (numero <= 100)) then
+writeln(‘ você ACERTOU’)
+else
+writeln(‘ você ERROU’);
+readkey;
+End.
