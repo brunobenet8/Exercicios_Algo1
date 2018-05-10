feafb94eaf4c077e52f348374b8e02138c21cb83
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-10 16:24:52 -0300

    Create Exemplo_decisao_2.pas
    Exemplo página 118, livro Algoritmo e Programação 1, UEPG;

---
+++ Exemplo_decisao_2.pas
@@ -0,0 +1,21 @@
+Program Exemplo_decisao_2;
+
+Var
+N1, N2, Media : real;
+
+Begin
+write(‘Entre com a primeira nota: ‘);
+read (N1);
+	
+write(‘Entre com a segunda
+read (N2);
+nota: ‘);
+Media := (N1 + N2) / 2;
+writeln;
+if (Media >= 7)then
+	 write(‘-> Aprovado’)
+else
+write(‘-> Reprovado’);
+readkey;
+	
+End.
