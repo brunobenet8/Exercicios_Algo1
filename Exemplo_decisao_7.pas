87e6e301ca65a95c4008a9d5c47a075d54b5ee36
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-10 16:30:32 -0300

    Create Exemplo_decisao_7.pas
    Exemplo página 126 livro Algoritmo e Programação, UEPG;

---
+++ Exemplo_decisao_7.pas
@@ -0,0 +1,15 @@
+Program Exemplo_decisao_7;
+
+Var
+letra : char;
+
+Begin
+write (‘ Tecle a consoante da palavra “aula” -> ‘);
+letra := readkey;
+if (letra = ‘l’) then
+write (‘ você ACERTOU - letra L)
+else
+write (‘ você ERROU ‘);
+readkey;
+	
+End.
