b35c9116d2f5678339ed762b35e9e2fa18cb22e0
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-10 16:27:03 -0300

    Create Exemplo_decisao_4.pas
    Exemplo página 122 livro Algoritmo e Programação 1, UEPG;

---
+++ Exemplo_decisao_4.pas
@@ -0,0 +1,15 @@
+Program Exemplo_decisao_4 ;
+
+Var
+numero : integer;
+
+Begin
+write (‘Entre com um número positivo menor do que 100: ‘);
+read(numero);
+if (numero >= 0) then
+if (numero <= 100) then
+writeln(‘ você ACERTOU’)
+else
+writeln(‘ você ERROU’);
+readkey;
+End.
