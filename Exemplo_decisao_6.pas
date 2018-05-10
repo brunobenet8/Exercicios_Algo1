4f63b7d91394addd839e08263cea085c6ccfcadc
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-10 16:29:20 -0300

    Create Exemplo_decisao_6.pas
    Exemplo página 125 livro Algoritmo e Programação 1, UEPG;

---
+++ Exemplo_decisao_6.pas
@@ -0,0 +1,21 @@
+Program Exemplo_decisao_6;
+
+Var
+numero : integer;
+
+Begin
+write(‘Digite um número: ‘);
+read(numero);
+Program Exemplo_decisao_6;
+if (numero < 0) then
+writeln (‘valor menor que zero’)
+else if (numero < 10) then
+	 writeln (‘valor => 0 e < 10’)
+else if (numero < 100) then
+		
+writeln (‘valor => 10 e < 100’)
+else
+	 writeln (‘valor => 100’);
+		
+readkey;
+End.
