3a8130820a4ecf59fed3ef7be4f90330285f1fe5
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-10 16:25:54 -0300

    Create Exemplo_decisao_3.pas
    Exemplo página 120, livro Algoritmo e Programação 1, UEPG;

---
+++ Exemplo_decisao_3.pas
@@ -0,0 +1,21 @@
+Program Exemplo_decisao_3;
+
+Var
+a,b,c : integer;
+
+Begin
+a:=1;
+b:=2;
+if (a<b) then
+Begin
+a:=b;
+c:=2;
+end
+else
+Begin
+b:=a;
+c:=4;
+end;
+write(‘Valores de a,b,c são: ‘,a,b,c);	
+readkey;
+End.
