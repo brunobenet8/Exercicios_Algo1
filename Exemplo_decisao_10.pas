173d8345ca2f71b6d7644d9bd19455acf71d0887
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-10 16:37:36 -0300

    Update Exemplo_decisao_10.pas

--- Exemplo_decisao_10.pas
+++ Exemplo_decisao_10.pas
@@ -6,12 +6,13 @@ letra : char;
 Begin
 write (‘Tecle uma letra entre a e z: ‘);
 letra := readkey;
-Program Exemplo_decisao_10;
+
 if ((letra >= ‘A’ ) and (letra <= ‘Z’)) then
 writeln(‘ você teclou - ‘,letra,’ - MAIÚSCULA’)
 else if ((letra >= ‘a’ ) and (letra <= ‘z’)) then
 writeln(‘ você teclou - ‘,letra,’ - minúscula’)
 else
 writeln(‘ Caractere inválido, não é uma letra....’);
+
 readkey;
 End.
