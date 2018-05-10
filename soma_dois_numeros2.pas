bdf8d0a2ef84314c6ac0cbb7efc342d9371e4d32
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-10 16:18:42 -0300

    Create soma_dois_numeros2.pas
    Exemplo página 107, livro Algoritmos e Programação 1, UEPG;

---
+++ soma_dois_numeros2.pas
@@ -0,0 +1,14 @@
+Program SOma_dois_numeros;
+
+var
+N1, N2, Soma : real;
+
+Begin
+writeln('Programa que soma dois números');
+write(Entre com o primeiro número: ');
+readln(N1);
+write('Entre com o segundo número: ');
+readln(N2);
+Soma := N1 + N2;
+write('A soma entre ', N1:5:2, ' mais ', N2:5:2, 'é igual a ', Soma:5:2);
+End.
