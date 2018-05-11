4f2a9dcf1d83efa287f92624f506a91d77b37131
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-11 00:22:52 -0300

    Update Exemplo_decisao_8.pas

--- Exemplo_decisao_8.pas
+++ Exemplo_decisao_8.pas
@@ -3,12 +3,12 @@ Program Exemplo_decisao_8;
 Var
 letra : char;
 Begin
-write (‘ Tecle a consoante da palavra “aula” -> ‘);
+write (' Tecle a consoante da palavra “aula” -> ');
 letra := readkey;
-if ((letra = ‘l’) or (letra = ‘L’)) then
-write (‘ você ACERTOU - letra L’)
+if ((letra = 'l') or (letra = 'L')) then
+write (' você ACERTOU - letra L')
 else
-write (‘ você ERROU ‘);
+write (' você ERROU ');
 readkey;
 	
 End.
