16411f464ece2bc5a7fa816408de374998bc5bb6
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-11 00:23:32 -0300

    Update Exemplo_decisao_7.pas

--- Exemplo_decisao_7.pas
+++ Exemplo_decisao_7.pas
@@ -4,12 +4,12 @@ Var
 letra : char;
 
 Begin
-write (‘ Tecle a consoante da palavra “aula” -> ‘);
+write (' Tecle a consoante da palavra “aula” -> ');
 letra := readkey;
-if (letra = ‘l’) then
-write (‘ você ACERTOU - letra L)
+if (letra = 'l') then
+write (' você ACERTOU - letra L')
 else
-write (‘ você ERROU ‘);
+write (' você ERROU ');
 readkey;
 	
 End.
