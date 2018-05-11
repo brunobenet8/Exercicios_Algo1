396287f7c6a1ca2f61d1be33e5ec0f0b36d89d72
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-11 00:25:24 -0300

    Update Exemplo_decisao_4.pas

--- Exemplo_decisao_4.pas
+++ Exemplo_decisao_4.pas
@@ -4,12 +4,12 @@ Var
 numero : integer;
 
 Begin
-write (‘Entre com um número positivo menor do que 100: ‘);
+write ('Entre com um número positivo menor do que 100: ');
 read(numero);
 if (numero >= 0) then
 if (numero <= 100) then
-writeln(‘ você ACERTOU’)
+writeln(' você ACERTOU')
 else
-writeln(‘ você ERROU’);
+writeln(' você ERROU');
 readkey;
 End.
