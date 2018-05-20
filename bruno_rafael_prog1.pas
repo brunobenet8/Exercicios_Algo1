4ddb0b4cc3a5c2248399e6d6d408349e4e0df18d
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-20 00:06:01 -0300

    Create bruno_rafael_prog1.pas

---
+++ bruno_rafael_prog1.pas
@@ -0,0 +1,21 @@
+program HelloWorld;
+
+var
+entrada : char;
+begin
+    entrada := 'f';
+    if((entrada >= 'A') and (entrada <= 'Z'))then
+        if((entrada = 'A') or (entrada = 'E') or (entrada = 'I') or (entrada = 'O') or (entrada = 'U'))then
+            writeln('Vogal Maiuscula!')
+        else
+            writeln('Consoante Maiuscula!')
+    else if ((entrada >= 'a') and (entrada <= 'z'))then
+        if((entrada = 'a') or (entrada = 'e') or (entrada = 'i') or (entrada = 'o') or (entrada = 'u'))then
+            writeln('Vogal Minuscula!')
+        else
+            writeln('Consoante Minuscula!')
+    else if((entrada >= '0') and (entrada <= '9'))then
+        writeln('Numero!')
+    else
+        writeln('Caractere especial!');
+end.
