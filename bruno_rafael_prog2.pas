c3b4075774fda117bc9c7813ea0bdcdd0618ff2c
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-24 12:10:41 -0300

    Update bruno_rafael_prog2.pas

--- bruno_rafael_prog2.pas
+++ bruno_rafael_prog2.pas
@@ -3,7 +3,9 @@ program bruno_rafael_prog2;
 var
 num,  nnum : integer;
 begin
-num:= 111;
+writeln('Digite um número de 3 algarismo:');
+read(num);
 nnum := (num * 10) + (trunc(num / 100) + (trunc((trunc(num / 10))mod(10)) * 3) + (trunc((num)mod(10)) * 5))mod(7);
+writeln('O novo número é:');
 writeln(nnum);
 end.
