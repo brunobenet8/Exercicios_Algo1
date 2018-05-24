aeefb1841fc23a9f36a2f87603c164bc980f1a5d
brunobenet8 <bruno.rafaelbenet@gmail.com>
2018-05-24 12:08:55 -0300

    Update bruno_rafael_prog1.pas

--- bruno_rafael_prog1.pas
+++ bruno_rafael_prog1.pas
@@ -3,6 +3,8 @@ program bruno_rafael_prog1;
 var
 entrada : char;
 begin
+writeln('Tecle uma vez:');
+readkey(entrada);
     entrada := 'f';
     if((entrada >= 'A') and (entrada <= 'Z'))then
         if((entrada = 'A') or (entrada = 'E') or (entrada = 'I') or (entrada = 'O') or (entrada = 'U'))then
